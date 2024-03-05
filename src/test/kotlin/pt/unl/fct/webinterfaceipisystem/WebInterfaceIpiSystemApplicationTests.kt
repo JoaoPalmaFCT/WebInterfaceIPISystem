82d41772286f8a92c3bdc979343865e9ae49c7d7
João Palma <jms.palma@campus.fct.unl.pt>
2024-03-05 15:33:10 +0000

    Initial commit

---
+++ src/test/kotlin/pt/unl/fct/webinterfaceipisystem/WebInterfaceIpiSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package pt.unl.fct.webinterfaceipisystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebInterfaceIpiSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
